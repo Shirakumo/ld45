b8854a1eeb5fa0c674d611267a038dae5e82bcda
Shinmera <shinmera@tymoon.eu>
2019-10-05 14:14:34 +0200

    wall has bsize

--- wall.lisp
+++ wall.lisp
@@ -5,7 +5,11 @@
 
 (define-shader-entity wall (vertex-entity located-entity solid)
   ((size :initarg :size :initform (vec 256 64) :accessor size)
+   (bsize :initform (vec 128 32) :accessor bsize)
    (vertex-array :initform (asset 'ld45 'unit))))
+
+(defmethod (setf size) :after (size (wall wall))
+  (vsetf (bsize wall) (/ (vx size) 2) (/ (vy size) 2)))
 
 (defmethod paint :before ((wall wall) target)
   (scale-by (vx (size wall)) (vy (size wall)) 1))
