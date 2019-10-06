362d19cb0ac4a4c8d185a43343e0ec7505d55734
Shinmera <shinmera@tymoon.eu>
2019-10-06 22:17:39 +0200

    Minor

--- effects.lisp
+++ effects.lisp
@@ -18,11 +18,8 @@
         (vao (vertex-array screen)))
     (setf (uniform program "strength") (float (- 1 (fade-timer screen)) 0f0))
     (gl:bind-texture :texture-2d (gl-name (texture screen)))
-    (with-pushed-attribs
-      (disable :depth-test)
-      (gl:bind-vertex-array (gl-name vao))
-      (%gl:draw-elements :triangles (size vao) :unsigned-int (cffi:null-pointer))
-      (gl:bind-vertex-array 0))))
+    (gl:bind-vertex-array (gl-name vao))
+    (%gl:draw-elements :triangles (size vao) :unsigned-int (cffi:null-pointer))))
 
 (define-class-shader (screen :vertex-shader)
   "layout(location = 0) in vec3 position;
