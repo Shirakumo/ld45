4d41c948120eb3f2fe85804249ffa777bd1b6da6
Shinmera <shinmera@tymoon.eu>
2019-10-06 20:41:26 +0200

    Fade game over

--- effects.lisp
+++ effects.lisp
@@ -5,13 +5,18 @@
   :min-filter :nearest
   :mag-filter :nearest)
 
-(define-shader-entity screen ()
-  ((vertex-array :initform (asset 'trial 'trial::fullscreen-square) :accessor vertex-array)
+(define-shader-subject screen ()
+  ((fade-timer :initform 1f0 :accessor fade-timer)
+   (vertex-array :initform (asset 'trial 'trial::fullscreen-square) :accessor vertex-array)
    (texture :initarg :texture :accessor texture)))
+
+(define-handler (screen tick) (ev dt)
+  (setf (fade-timer screen) (max 0 (- (fade-timer screen) dt))))
 
 (defmethod paint ((screen screen) (pass shader-pass))
   (let ((program (shader-program-for-pass pass screen))
         (vao (vertex-array screen)))
+    (setf (uniform program "strength") (float (- 1 (fade-timer screen)) 0f0))
     (gl:bind-texture :texture-2d (gl-name (texture screen)))
     (with-pushed-attribs
       (disable :depth-test)
@@ -30,9 +35,12 @@ void main(){
 
 (define-class-shader (screen :fragment-shader)
   "uniform sampler2D screen;
+uniform float strength = 0.0;
 in vec2 tex_coord;
 out vec4 color;
 
 void main(){
-  color = texture(screen, tex_coord);
+  color = texture(screen, tex_coord)*strength;
 }")
+
+
